90744901f3826dc836616f02199cd160a7abc80b
igor alves <112185065+igorcalves@users.noreply.github.com>
2024-02-13 03:25:18 -0300

    first Commit

---
+++ RegisterPoint/src/main/java/br/com/igor/registerPoint/RegisterPoint/RegisterPointApplication.java
@@ -0,0 +1,13 @@
+package br.com.igor.registerPoint.RegisterPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterPointApplication.class, args);
+	}
+
+}
---
+++ RegisterPoint/src/test/java/br/com/igor/registerPoint/RegisterPoint/RegisterPointApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igor.registerPoint.RegisterPoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
