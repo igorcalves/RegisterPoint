8d84904522ece5bd47ad31cfb846f06e3137d308
igor alves <igor193@live.com>
2024-02-24 06:24:06 +0000

    init rp controller

---
+++ RegisterPoint/src/main/java/br/com/igor/registerPoint/controllers/RegisterPointController.java
@@ -0,0 +1,15 @@
+package br.com.igor.registerPoint.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestController
+@RequestMapping("users/rp")
+public class RegisterPointController {
+
+  
+    
+}
